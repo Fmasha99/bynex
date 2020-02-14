84c1af3b1a9672fbc553d909b177ce7b99e56dbc
Филипченко Мария Витальевна <filipchanka@sc.softclub.by>
2020-02-14 17:26:17 +0300

    add events to form's buttons

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ window.addEventListener('load', () => {
     let openMenu = document.getElementsByClassName('menu-button')[0];
     let close = document.getElementsByClassName('close')[0];
     let hamburgerMenu = document.getElementsByClassName('hamburger-menu')[0];
-    let menuItem = document.getElementsByClassName('suggest-menu');
-    let registration = document.getElementById('registr');
-    let enter = document.getElementById('enter');
-    let form = document.getElementsByClassName('form')[0];
 
     openMenu.addEventListener('click', () => {
         hamburgerMenu.style.display = 'block';
@@ -15,17 +11,6 @@ window.addEventListener('load', () => {
     close.addEventListener('click', () => {
         hamburgerMenu.style.display = 'none';
     });
-
-    for (let index = 0; index < menuItem.length; index++) {
-        menuItem[index].addEventListener('click', () => {
-            hamburgerMenu.style.display = 'none';
-        } )
-
-    }
-
-    // menuItem.addEventListener ('click', () => {
-    //     hamburgerMenu.style.display = 'none';
-    // });
 }); 
 
 function openWindow(a) {
@@ -33,11 +18,16 @@ function openWindow(a) {
     let form = document.getElementsByClassName('form')[0];
     let formName = document.getElementById('form-name');
     let button = document.getElementById('enter-form');
+    let closeForm = document.getElementById('close-button');
 
 
     form.style.display = 'block';
     formName.innerHTML = a;
     button.innerHTML = a;
+
+    closeForm.addEventListener ('click', () => {
+        form.style.display = 'none';
+    });
 
 
 }
