1c074ba094fdf60ffcec2b111493777464b8913d
Филипченко Мария Витальевна <filipchanka@sc.softclub.by>
2020-02-18 10:05:29 +0300

    create enter/registration forms with js

--- script.js
+++ script.js
@@ -22,71 +22,41 @@ function pdOpenWindows (a, content) {
     let pdInnerForm = document.getElementById('pdInnerForm');
     
     if (content === 1) {
-        
+        formName = 'Вход';
+        placeholderLogin = 'Введите логин';
+        placeholderPassword = 'Введите пароль';
+        button = 'Войти';
     } else if (content === 2) {
-        
+        formName = 'Регистрация';
+        placeholderLogin = 'Придумайте логин';
+        placeholderPassword = 'Придумайте пароль';
+        button = 'Зарегистрироваться';
     }
     
     if (a) {
         pdInnerForm.innerHTML = `
             <section class="form">
                 <div class="form-options">
-                    <span class="form-name" id="form-name"></span>
+                    <span class="form-name" id="form-name">${formName}</span>
                     <img src="./assets/images/cancel.png" alt="" class="form-image" id="close-button" onclick="closeForm()">
                 </div>
                 <div class="enter-options">
-                    <p class="placeholder-login" id="placeholder-login"></p>
+                    <p class="placeholder-login" id="placeholder-login">${placeholderLogin}</p>
                     <input type="text" class="login" id="login">
-                    <p class="placeholder-password" id="placeholder-password"></p>
+                    <p class="placeholder-password" id="placeholder-password">${placeholderPassword}</p>
                     <div class="password-container">
                         <input type="password" class="password" id="password">
                         <img src="./assets/images/show.png" id="show-password" onclick="switchPassword()">
                     </div>
-                    <button type="submit" class="enter-form" id='enter-form' onclick="closeForm()"></button>
+                    <button type="submit" class="enter-form" id='enter-form' onclick="closeForm()">${button}</button>
                 </div>
             </section> 
 
-            <div style="position: fixed; top: 0; left: 0; bottom: 0; right: 0; background: rgba(0, 0, 0, 0.301); z-index: 100;" onclick="pdOpenWindows(false)">
-
-            </div>
+            <div style="position: fixed; top: 0; left: 0; bottom: 0; right: 0; background: rgba(0, 0, 0, 0.301); z-index: 100;" onclick="pdOpenWindows(false)"></div>
         `
     } else {
-        pdInnerForm.innerHTML = ''
+        pdInnerForm.innerHTML = '';
     }
-}
-
-function openWindow() {
-    console.log()
-    let form = document.getElementsByClassName('form')[0];
-    let formName = document.getElementById('form-name');
-    let placeholderLogin = document.getElementById('placeholder-login');
-    let placeholderPassword = document.getElementById('placeholder-password');
-    let password = document.getElementById('password');
-    let login = document.getElementById('login');
-    let showPassword = document.getElementById('show-password');
-
-    password.type = "password";
-    showPassword.src = "../assets/images/show.png";
-
-    form.style.display = 'block';
-    
-
-    if (a === 1) {
-        placeholderLogin.innerHTML = "Введите логин";
-        placeholderPassword.innerHTML = "Введите пароль";
-        formName.innerHTML = a;
-        button.innerHTML = a;
-    } else if (a === 2) {
-        placeholderLogin.innerHTML = "Придумайте логин";
-        placeholderPassword.innerHTML = "Придумайте пароль";
-    }
-
-    // formName.innerHTML = 'Регистрация';
-    // button.innerHTML = 'Регистрация';
-    // placeholderLogin.innerHTML = "Придумайте логин";
-    // placeholderPassword.innerHTML = "Придумайте пароль";
-
-    
 }
 
 function switchPassword () {
